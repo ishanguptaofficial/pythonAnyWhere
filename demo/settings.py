c84ea54d068e320a4264fedaeb7b84d6c33d6984
ishanguptaMonk <ishan.gupta@digimonk.in>
2023-09-08 23:12:49 +0530

    commit all file

--- demo/settings.py
+++ demo/settings.py
@@ -108,9 +108,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = ''
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
